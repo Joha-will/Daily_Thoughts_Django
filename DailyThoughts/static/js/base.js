ec4b3e9635531bb0719529b959758c13c60d96ef
Joha-will <johawilsonatu@icloud.com>
2023-11-26 22:54:46 +0000

    Add static files to DailyThoughts

---
+++ DailyThoughts/static/js/base.js
@@ -0,0 +1,7 @@
+// Hide django flash messages after few seconds
+
+var messageTimer = document.getElementById('message-timer');
+
+setTimeout(function() {
+    messageTimer.style.display = 'none';
+}, 3000);
